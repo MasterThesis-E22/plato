86d227ca172acc3d60f7aa55d8319ce495ee96ee
Harfeld <andreas.harfeld@gmail.com>
2022-11-04 14:06:10 +0100

    added ymls and started scripting bash execution

---
+++ experiments/sync/run_embryo_lr_experiments.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+base_path=$(dirname "$0")            # relative
+base_path=$(cd "$MY_PATH" && pwd)    # absolutized and normalized
+if [[ -z "$base_path" ]] ; then
+  # error; for some reason, the path is not accessible
+  # to the script (e.g. permissions re-evaled after suid)
+  exit 1  # fail
+fi
+echo "$base_path"
+
+experiments=("fedavg_r100_e1_lr0000001" "fedavg_r100_e1_lr000001" "fedavg_r100_e1_lr00001")
+for experiment in ${experiments}; do
+    if [ ! -f output/$experiment.txt ]; then
+        touch $base_path/experiment_output/$experiment.txt
+    fi
+    echo "success"
+    ##../../venv/bin/python sync_base.py -c $experiment.yml > output/$experiment.txt
+done
