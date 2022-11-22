0d0b81ef51d6a094c38bb3e1046d5307b98f1dd0
Harfeld <andreas.harfeld@gmail.com>
2022-11-22 09:24:34 +0100

    Added snippet for possible reproduction

--- experiments/sync/sync_base.py
+++ experiments/sync/sync_base.py
@@ -2,9 +2,18 @@ from plato.clients import simple
 from plato.datasources import base
 from plato.servers import fedavg
 from plato.trainers import basic
+import numpy as np
 
 
 def main():
+    import torch
+    random_seed = 69 # or any of your favorite number 
+    torch.manual_seed(random_seed)
+    torch.cuda.manual_seed(random_seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    np.random.seed(random_seed)
+    
     client = simple.Client()
     server = fedavg.Server()
     server.run(client)
