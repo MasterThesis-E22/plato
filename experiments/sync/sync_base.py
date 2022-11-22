8bdd14e62752c0318078478dd3e96efe6f40e8c2
Harfeld <andreas.harfeld@gmail.com>
2022-11-22 09:25:13 +0100

    fix

--- experiments/sync/sync_base.py
+++ experiments/sync/sync_base.py
@@ -2,11 +2,12 @@ from plato.clients import simple
 from plato.datasources import base
 from plato.servers import fedavg
 from plato.trainers import basic
+import torch
 import numpy as np
 
 
 def main():
-    import torch
+    
     random_seed = 69 # or any of your favorite number 
     torch.manual_seed(random_seed)
     torch.cuda.manual_seed(random_seed)
